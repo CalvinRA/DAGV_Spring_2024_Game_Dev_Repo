8a5540137d06a3d787eb3688954aae1a64fd7221
CalvinRA <97717845+CalvinRA@users.noreply.github.com>
2024-01-24 22:34:15 -0700

    Working On Lab

---
+++ TestProject1/Assets/BegginerSriptableObjs/BegginerTextLabelBehavoir.cs
@@ -0,0 +1,19 @@
+using System;
+using System.Collections;
+using System.Collections.Generic;
+using System.Globalization;
+using UnityEngine;
+using UnityEngine.UI;
+
+[RequireComponent(typeof(Text))]
+public class BegginerTextLabelBehavoir : MonoBehaviour
+{
+    public Text lable;
+    public BegginerFloatData dataObj;
+
+    public void Start()
+    {
+        lable = GetComponent<Text>();
+        lable.text = dataObj.value.ToString(CultureInfo.InvariantCulture);
+    }
+}
